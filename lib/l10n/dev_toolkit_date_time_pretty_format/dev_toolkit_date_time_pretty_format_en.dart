ac4d963dae082db79e769275c73814a64d041fca
Salih Agić <salih.a@onedigital.group>
2025-01-22 16:04:27 +0100

    Translations updated

--- lib/l10n/dev_toolkit_date_time_pretty_format/dev_toolkit_date_time_pretty_format_en.dart
+++ lib/l10n/dev_toolkit_date_time_pretty_format/dev_toolkit_date_time_pretty_format_en.dart
@@ -44,39 +44,39 @@ class DevToolkitDateTimePrettyFormatLocalizationsEn implements DevToolkitDateTim
   @override
   String get just_now_short => 'Now';
   @override
-  String get pretty_format_in_1_second => 'NOT_TRANSLATED';
+  String get pretty_format_in_1_second => 'In 1 second';
   @override
-  String get pretty_format_short_in_1_second => 'NOT_TRANSLATED';
+  String get pretty_format_short_in_1_second => 'In 1s';
   @override
-  String pretty_format_in_few_seconds(String amount) => 'NOT_TRANSLATED';
+  String pretty_format_in_few_seconds(String amount) => 'In $amount seconds';
   @override
-  String pretty_format_short_in_few_seconds(String amount) => 'NOT_TRANSLATED';
+  String pretty_format_short_in_few_seconds(String amount) => '$amount seconds';
   @override
-  String get pretty_format_in_1_minute => 'NOT_TRANSLATED';
+  String get pretty_format_in_1_minute => 'In 1 minute';
   @override
-  String get pretty_format_short_in_1_minute => 'NOT_TRANSLATED';
+  String get pretty_format_short_in_1_minute => 'In 1m';
   @override
-  String pretty_format_in_few_minutes(String amount) => 'NOT_TRANSLATED';
+  String pretty_format_in_few_minutes(String amount) => 'In $amount minutes';
   @override
-  String pretty_format_short_in_few_minutes(String amount) => 'NOT_TRANSLATED';
+  String pretty_format_short_in_few_minutes(String amount) => 'In $amount min';
   @override
-  String get pretty_format_in_1_hour => 'NOT_TRANSLATED';
+  String get pretty_format_in_1_hour => 'In 1 hour';
   @override
-  String get pretty_format_short_in_1_hour => 'NOT_TRANSLATED';
+  String get pretty_format_short_in_1_hour => 'In 1h';
   @override
-  String pretty_format_in_few_hours(String amount) => 'NOT_TRANSLATED';
+  String pretty_format_in_few_hours(String amount) => 'In $amount hours';
   @override
-  String pretty_format_short_in_few_hours(String amount) => 'NOT_TRANSLATED';
+  String pretty_format_short_in_few_hours(String amount) => 'In ${amount}h';
   @override
-  String pretty_format_tomorrow_at(String amount) => 'NOT_TRANSLATED';
+  String pretty_format_tomorrow_at(String amount) => 'Tomorrow at $amount';
   @override
-  String pretty_format_short_tomorrow_at(String amount) => 'NOT_TRANSLATED';
+  String pretty_format_short_tomorrow_at(String amount) => 'Tomorrow at $amount';
   @override
-  String get pretty_format_in_1_day => 'NOT_TRANSLATED';
+  String get pretty_format_in_1_day => 'In 1 day';
   @override
-  String get pretty_format_short_in_1_day => 'NOT_TRANSLATED';
+  String get pretty_format_short_in_1_day => 'In 1d';
   @override
-  String pretty_format_in_few_days(String amount) => 'NOT_TRANSLATED';
+  String pretty_format_in_few_days(String amount) => 'In $amount days';
   @override
-  String pretty_format_short_in_few_days(String amount) => 'NOT_TRANSLATED';
+  String pretty_format_short_in_few_days(String amount) => 'In ${amount}d';
 }
